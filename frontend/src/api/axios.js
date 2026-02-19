ea2a77ac041f55c647a2b9ee70a51ea5e47ca28d
hhsksonu <sonukumaryz463@gmail.com>
2026-02-19 06:42:36 +0530

    bug fixes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE = 'http://localhost:8000/api';
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || '/api',
+});
 
 const api = axios.create({ baseURL: BASE, headers: { 'Content-Type': 'application/json' } });
 
